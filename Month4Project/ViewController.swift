ce6e3445300bf7b1612b86bd8edd41c8f4fa87b2
Almaz Sultanbai <aldeco312@gmail.com>
2024-03-10 18:06:08 +0600

    Initial Commit

---
+++ Month4Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Month4Project
+//
+//  Created by Sultanbai Almaz on 10/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
